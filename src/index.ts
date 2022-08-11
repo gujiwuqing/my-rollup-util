42eef8bd72dfacb91b3f0041e34f097789d6019e
孤寂无情 <15279279713@163.com>
2022-08-11 09:46:48 +0800

    新增自动化部署到npm

--- src/index.ts
+++ src/index.ts
@@ -31,3 +31,34 @@ export const getClient = () => {
     return "pc";
   }
 };
+
+/**
+ * 获取类型
+ * @param val
+ * @returns
+ */
+export const myTypeof = (val) => {
+  var type = typeof val;
+  var resObj = {
+    "[object Array]": "array",
+    "[object Map]": "map",
+    "[object Set]": "set",
+    "[object Object]": "object",
+    "[object Number]": "object number",
+    "[object String]": "object string",
+    "[object Boolean]": "object boolean",
+  };
+  if (val === null) {
+    return "null";
+  } else if (type === "object") {
+    var typeStr = Object.prototype.toString.call(val);
+    return resObj[typeStr];
+  } else {
+    return type;
+  }
+};
+
+// 数组去重
+export const unique = (arr: any[]) => {
+  return Array.from(new Set(arr));
+}
